7f758231a200da3c8d6ab27b33916135fd778041
Sean Fraga <seanfraga@gmail.com>
2023-07-06 10:00:03 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// Load the messages asynchronously from the CSV file
+async function loadMessages() {
+  try {
+    const response = await fetch('messages.csv');
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const csvData = await response.text();
+    const messages = Papa.parse(csvData, { header: true, skipEmptyLines: true }).data;
+
+    // Generate a random index to select a message
+    const randomIndex = Math.floor(Math.random() * messages.length);
+    const selectedMessage = messages[randomIndex]?.message;
+
+    if (selectedMessage) {
+      document.getElementById("message").textContent = selectedMessage;
+    } else {
+      throw new Error("No message selected");
+    }
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+}
+
+// Call the loadMessages function when the HTML content is fully loaded
+document.addEventListener('DOMContentLoaded', loadMessages);
